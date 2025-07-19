62b8c3cd98d9db6c61348a5a7daf0391479d5810
kartikeye <kartikeye.dangwal@gmail.com>
2025-07-20 02:27:18 +1200

    props practice

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -1,17 +1,18 @@
 // let language = 'TypeScript';
 // let moon = 'Moon';
 
-function Header() {
+function Header(props) {
+  console.log(props)
     return (
         <header>
-            <h1>Welcome to Header</h1>
+            <h1>Welcome {props.name} with age is {props.age}</h1>
         </header>
     );
   }
 function App() {
     return (
     <div>
-    <Header /> 
+    <Header name="Kartikeye" age = {24}/> 
     <main>
         <h1>Welcome to React Project</h1>
         </main>
